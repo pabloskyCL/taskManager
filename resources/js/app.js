b3c1661818ff557a791af2ae53766af7ed3d373c
Pablo Quiroz <pabloskiquiroz@gmail.com>
2023-11-30 11:38:19 -0300

    environment ready

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import test from './components/Test.vue';
+
+createApp(test).mount('#app');
---
+++ resources/js/components/Test.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>test environtment</h2>
+</template>
