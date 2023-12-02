6920cd9936468955c8c6eb550935896ef26f280d
Pablo Quiroz <pabloskiquiroz@gmail.com>
2023-12-01 23:25:54 -0300

    feat: add test for authentication and tasks

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->text(15),
+            'user_id' => 1,
+            'completed' => 1, // password
+        ];
+    }
+}
